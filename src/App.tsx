46e43675543ce5b4c26396b8a72b23e6397ed0fa
paula.bass.werner <paula.bass.werner@gmail.com>
2024-09-03 15:11:38 -0400

    Labs Work A1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="Labs" />} />
+        <Route path="/Labs/*" element={<Labs />} />
+      </Routes>
+    </HashRouter>
   );
 }
 export default App;
