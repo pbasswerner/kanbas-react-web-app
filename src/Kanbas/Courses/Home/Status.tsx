75c703096ecaa88b828aecfcb5ac5a66b0f893b6
paula.bass.werner <paula.bass.werner@gmail.com>
2024-09-04 11:45:15 -0400

    Modules and Course Home Screen

---
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -0,0 +1,38 @@
+export default function CourseStatus() {
+    return (
+        <div id="wd-course-status">
+            <h2>Course Status</h2>
+            <div>
+                <button>Unpublish</button>
+                <button>Publish</button>
+            </div>
+            <div>
+                <button>Import Existing Content</button>
+            </div>
+
+            <div>
+                <button>Import from Commons</button>
+            </div>
+
+            <div>
+                <button>Choose from Home Page</button>
+            </div>
+            <div>
+                <button>View Course Stream</button>
+            </div>
+
+            <div>
+                <button>New Announcements</button>
+            </div>
+
+            <div>
+                <button>New Analytics</button>
+            </div>
+
+            <div>
+                <button>View Course Notifications</button>
+            </div>
+
+        </div>
+    );
+}
