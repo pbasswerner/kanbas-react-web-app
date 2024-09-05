c3a541ecf2e2884f5c2e59bbae2b0bf7eea12bf7
paula.bass.werner <paula.bass.werner@gmail.com>
2024-09-05 09:56:58 -0400

    Assignments Page

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -2,6 +2,7 @@ import CoursesNavigation from "./Navigation";
 import { Navigate, Route, Routes } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 export default function Courses() {
     return (
         <div id="wd-courses">
@@ -17,7 +18,7 @@ export default function Courses() {
                             <Route path="/" element={<Navigate to="Home" />} />
                             <Route path="Home" element={<Home />} />
                             <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
+                            <Route path="Assignments" element={<Assignments />} />0-op
                             <Route path="Assignments/:id" element={<h2>Assignment Editor</h2>} />
                         </Routes>
                     </td>
